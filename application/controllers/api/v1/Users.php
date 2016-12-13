41edbe560aa6d6756dceec8985feb552ed0f9650
Alvin Oktavianus <agentsam1995@gmail.com>
2016-12-13 13:59:05 +0700

    Add CORS filter to constructor

--- application/controllers/api/v1/Users.php
+++ application/controllers/api/v1/Users.php
@@ -6,6 +6,8 @@ class Users extends REST_Controller {
 
     public function __construct()
     {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         parent::__construct();
         $this->load->model('users_model');
     }
