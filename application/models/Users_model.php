fd960f2deefe59ceda5c1f2121ee0864a0772688
Alvin Oktavianus <agentsam1995@gmail.com>
2016-12-16 15:39:38 +0700

    Fix users_model error

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -3,9 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users_model extends CI_Model {
 
-    public function get_users($emplid)
+    public function get_users($email)
+    {
+        $this->db->where('Email', $email);
+        return $this->db->get('Ms_Employee')->result();
+    }
+
+    public function get_users_by_emplid($emplid)
     {
         $this->db->where('EmployeeID', $emplid);
+        $this->db->select('EmployeeID, FullName');
         return $this->db->get('Ms_Employee')->result();
     }
 
